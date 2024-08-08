ee8e8104e0d1d9d81038c10c89ce922177aeb798
Narender <narender@Narenders-MacBook-Pro.local>
2024-08-09 00:59:02 +0530

    added currency converter project

--- week3_calculator/currency.py
+++ week3_calculator/currency.py
@@ -1,10 +1,10 @@
 import requests
 
-API_KEY = "masked"
+API_KEY = "MASKED_API_KEY"
 BASE_URL = f"https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}"
 
 
-CURRENCRIES = ["USD", "EUR", "GBP", "INR"]
+CURRENCRIES = ["USD", "EUR", "GBP", "INR", "JPY", "AUD", "CAD", "CHF"]
 
 
 def convert_currency(base):
@@ -14,10 +14,22 @@ def convert_currency(base):
         response = requests.get(url)
         data = response.json()
         return data["data"]
-    except Exception as e:
-        print(e)
+    except:
+        print("invalid currency")
         return None
 
 
-data = convert_currency("USD")
-print(data)
+while True:
+    base = input("Enter base currency: (press  q to quit) ").upper()
+
+    if base == "Q":
+        break
+
+    data = convert_currency(base)
+    if not data:
+        continue
+
+    del data[base]
+
+    for abbreviation, value in data.items():
+        print(f"{abbreviation}: {value}")
