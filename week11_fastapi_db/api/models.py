be4e94b04edb216125afec2fe5028931c981d6b7
Narender <Narendra096@outlook.com>
2024-11-17 23:06:31 +0530

    commiting checkpoint

--- week11_fastapi_db/api/models.py
+++ week11_fastapi_db/api/models.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, SQLModel, create_engine
+from sqlmodel import Field, SQLModel, create_engine, Session
 
 
 class Hero(SQLModel, table=True):
@@ -11,8 +11,38 @@ class Hero(SQLModel, table=True):
 db_name = "database.db"
 
 url = f"sqlite:///{db_name}"
-engine = create_engine(url, echo=True)
+engine = create_engine(url)
 
 
 def create_db_and_table():
     SQLModel.metadata.create_all(engine)
+
+
+def hero_insert():
+    hero_1 = Hero(name="deadpool", secret_name="dead")
+    hero_2 = Hero(name="wolverine", secret_name="wolf", age=54)
+    hero_3 = Hero(name="hulk", secret_name="disaster", age=55)
+
+    with Session(engine) as session:
+        session.add(hero_1)
+        session.add(hero_3)
+        session.add(hero_2)
+        session.commit()
+
+        print("values after commiting")
+        print("Hero 1", hero_1)
+
+        print("attributes after commiting changes")
+        print("Hero1_name", hero_1.name)
+
+        print("pring id of heros")
+        print("Hero1_id", hero_1.id)
+
+
+def main():
+    create_db_and_table()
+    hero_insert()
+
+
+if __name__ == "__main__":
+    main()
